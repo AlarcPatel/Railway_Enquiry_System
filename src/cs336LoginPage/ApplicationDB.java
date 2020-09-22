e7a2c7a7b1ee233aa8451400bbc29abd2c75793e
AlarcPatel <33972733+AlarcPatel@users.noreply.github.com>
2020-09-21 22:02:20 -0700

    Add files via upload

---
+++ src/cs336LoginPage/ApplicationDB.java
@@ -0,0 +1,61 @@
+package cs336LoginPage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import cs336LoginPage.ApplicationDB;
+
+public class ApplicationDB {
+	
+	public ApplicationDB(){
+		
+	}
+
+	public Connection getConnection(){
+		
+		//Create a connection string
+		String connectionUrl = "jdbc:mysql://group18-336.cpfupltk9l6g.us-east-2.rds.amazonaws.com:3306/RAILWAY_REGISTRATION_FINAL2";
+		Connection connection = null;
+		
+		try {
+			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			//Create a connection to your DB
+			connection = DriverManager.getConnection(connectionUrl,"Group18", "Rutgers2022");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return connection;
+		
+	}
+	
+	public void closeConnection(Connection connection){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		ApplicationDB dao = new ApplicationDB();
+		Connection connection = dao.getConnection();
+		
+		System.out.println(connection);		
+		dao.closeConnection(connection);
+	}
+}
